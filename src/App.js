476e48be9a0e03cddcbd0025a685b6de430e2007
Intrepid Panda <jerick999@gmail.com>
2018-09-11 07:32:15 -0600

    Make a function to format conditions data for use

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Banner from './Banner';
 import UI from './UI';
 import Forecast10Day from './Forecast10Day';
 import ForecastHourly from './ForecastHourly';
+import data from './data';
+// import key from './key';
 
 export default class App extends Component {
   constructor() {
@@ -25,6 +27,30 @@ export default class App extends Component {
     if (localStorage.getItem('locationFinal') !== null) {
       this.setState({locationFinal: localStorage.getItem('locationFinal')}); 
     }
+  }
+
+  getConditionsData () {
+    this.setState({conditionsData: {
+      city: data.response.current_observation.display_location.full
+      condition: data.response.current_observation.weather
+      weekday: data.response.forecast.simpleforecast.forecastday[0].date.weekday
+      month: data.response.forecast.simpleforecast.forecastday[0].date.monthname
+      date: data.response.forecast.simpleforecast.forecastday[0].date.day
+      temp: data.response.current_observation.temperature_string
+      high_f: data.response.forecast.simpleforecast.forecastday[0].high.fahrenheit
+      high_c: data.response.forecast.simpleforecast.forecastday[0].high.celsius
+      low_f: data.response.forecast.simpleforecast.forecastday[0].low.fahrenheit
+      low_c: data.response.forecast.simpleforecast.forecastday[0].low.celsius
+      summary: data.response.forecast.txt_forecast.forecastday[0].fcttext
+    }});
+  }
+
+  getForecastData () {
+
+  }
+
+  getHourlyData () {
+
   }
 
   handleLocationChange (event) {
