3f54b7c8874c6afe1d4efb09a6285739b50f44ac
Intrepid Panda <jerick999@gmail.com>
2018-09-13 10:13:24 -0600

    Set up files to accept the trie as one of their own

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './SearchForm.css';
 import largestCities from './largestCities';
+import Trie from '@jessica-erickson/complete-me'
 
 export default class SearchForm extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      trie: new Trie()
+    }
+
+    this.state.trie.populate(largestCities);
   }
 
   render () {
@@ -17,8 +24,14 @@ export default class SearchForm extends Component {
           type='text' 
           value={this.props.locationValue} 
           placeholder='Enter City, State or ZIP Code here!' 
-          onChange={this.props.onLocationChange} 
+          onChange={this.props.onLocationChange}
+          list='suggestions' 
         />
+        <datalist id='suggestions'>
+          {this.state.trie.suggestions(this.props.locationValue).slice(0, 10).map((suggestion, index) => {
+            return <option value={suggestion} key={index} />
+          })}
+        </datalist>
         <input 
           type='submit' 
           value='GO!' 
